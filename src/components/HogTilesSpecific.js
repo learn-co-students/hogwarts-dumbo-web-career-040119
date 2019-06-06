ed3abce8e6878e50a38d524840b70803ce91ca93
github username <github email address>
2019-06-06 17:09:06 -0400

    Done.

--- src/components/HogTilesSpecific.js
+++ src/components/HogTilesSpecific.js
@@ -3,7 +3,7 @@ import React, { Component, Fragment } from 'react'
 export default class HogTilesSpecific extends Component {
     state = {
         expand: false,
-        hide: false
+        hide: false,
     }
 
     handleClick = () => {
@@ -19,28 +19,28 @@ export default class HogTilesSpecific extends Component {
     }
 
     render() {
-        // console.log(this.props.hog)
+        // console.log(this.state.hide)
         return (
-        <div className= "ui eight wide column" onClick={this.handleClick}>
-            {/* <button onClick={this.handleClick}>Hide</button> */}
-            {
+        <div className= "ui eight wide column" id={this.state.hide ? "hide" : ""} >
+             <button onClick={this.handleHideClick}>Hide</button>
+            {  
                 this.state.expand ?
                 <Fragment>
-                    <h1>{this.props.hog.name}</h1>
+                    <h1 onClick={this.handleClick}>{this.props.hog.name}</h1>
                     {/* <img src={this.props.hog.image}></img>  */}
                     <h3>{this.props.hog.specialty}</h3>
                     <p>Greased? {this.props.hog.greased ? "No" : "Yes"}</p>
-                    <p>weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water: {this.props.hog[`weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water`]}</p>
+                    <p>Weight: {this.props.hog[`weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water`]}</p>
                     <p>Medal: {this.props.hog[`highest medal achieved`].toUpperCase()}</p>
                     
                 </Fragment>
                 :
                 <Fragment>
-                    <h1>{this.props.hog.name}</h1>
+                    <h1 onClick={this.handleClick}>{this.props.hog.name}</h1>
                     <img src={this.props.hog.image}></img> 
                 </Fragment>
             }
-
+           
         </div>
         )
     }
